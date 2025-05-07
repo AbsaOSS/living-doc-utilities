3d25634e528f49af960a087beebe3f1386e0edc8
miroslavpojer <miroslav.pojer@absa.africa>
2025-05-07 15:04:15 +0200

    Testing.

--- tests/conftest.py
+++ tests/conftest.py
@@ -20,7 +20,7 @@ from github import Github
 from github.Rate import Rate
 from github.RateLimit import RateLimit
 
-from tests.github import GithubRateLimiter
+from living_doc_utilities.github.rate_limiter import GithubRateLimiter
 
 
 @pytest.fixture
